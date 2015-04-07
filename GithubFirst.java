822c414b0e68f31f87fa5b6c348dcd408aef3a9d
rakianwar <raki.anwar.1994@gmail.com>
2015-04-07 08:22:46 +0700

    Untuk TP

---
+++ GithubFirst.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package githubfirst;
+
+/**
+ *
+ * @author KIKI
+ */
+public class GithubFirst {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello, nama saya Syam Fikry Mardiansyah. Nim saya 6306130047. dan saya suka sekali praktikum RPL MOD8 ini");
+    }
+    
+}
